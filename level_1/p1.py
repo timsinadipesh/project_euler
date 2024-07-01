760b91bff802728f0aa7024dc8d33abee4ce7f28
creasac2 <timsinadipesh77@gmail.com>
2024-07-01 18:27:08 +0545

    update

--- level_1/p1.py
+++ level_1/p1.py
@@ -1,9 +1,14 @@
 """
-sum of all the multiples of 3 or 5 below 1000
+sum of all the multiples of 3 or 5 upto 1000
 """
 
-total = 0
-for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        total += i
+def sum_divisibles_upto(target, divisor):
+    upperlimit = target // divisor
+    tot = 0
+    for i in range(upperlimit+1):
+        tot += i
+    return tot * divisor
+
+n = 999
+total =  sum_divisibles_upto(n, 3) + sum_divisibles_upto(n, 5) - sum_divisibles_upto(n, 15)
 print(total)
